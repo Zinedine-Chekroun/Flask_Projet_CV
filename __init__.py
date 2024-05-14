c3171d039813104a5f8aead05cf9feea8e585219
Zinedine-Chekroun <165790750+Zinedine-Chekroun@users.noreply.github.com>
2024-05-14 13:09:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, jsonify
+import openai
+
 
 app = Flask(__name__) #creating flask app name
 
+
+
+# Configuration de l'API OpenAI
+openai.api_key = 'sk-proj-tF6vKUTIOXfDxdkhHVhnT3BlbkFJ0ikNsj2QsfOsIm3DxRDU'
+
+
+
+# Route pour l'interaction avec le chatbot
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.form['user_message']
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=user_message,
+        temperature=0.7,
+        max_tokens=150
+    )
+    bot_reply = response.choices[0].text.strip()
+    return jsonify({'bot_reply': bot_reply})
+
+
 @app.route('/') #comm4
 def home():
-    return render_template("resume_1.html")
+    return render_template("resume_2.html")
 
 @app.route('/resume_1')
 def resume_1():
@@ -19,4 +42,4 @@ def resume_template():
     return render_template("resume_template.html")
 
 if(__name__ == "__main__"):
-    app.run()
+    app.run(debug=True)
